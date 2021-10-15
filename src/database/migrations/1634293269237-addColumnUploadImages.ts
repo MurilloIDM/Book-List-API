eb91c3ff9f4d406d8bf2779adad1b3ea6d6c8d52
MurilloIDM <murillo.idem@gmail.com>
2021-10-15 07:26:42 -0300

    Implementação das colunas de avatar e bookCover para upload de imagem

---
+++ src/database/migrations/1634293269237-addColumnUploadImages.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class addColumnUploadImages1634293269237 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "avatar",
+        type: "string",
+        isNullable: true,
+      })
+    );
+
+    await queryRunner.addColumn(
+      "books",
+      new TableColumn({
+        name: "bookCover",
+        type: "string",
+        isNullable: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "avatar");
+
+    await queryRunner.dropColumn("books", "bookCover");
+  }
+}
